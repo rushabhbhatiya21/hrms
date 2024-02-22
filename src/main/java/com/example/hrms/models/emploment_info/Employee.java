548ef09718ef6f83b04798c6064bc5eebb71d1dd
rushabhbhatiya21 <rushabhbhatiya1997@gmail.com>
2024-02-22 14:31:39 +0530

    second

---
+++ src/main/java/com/example/hrms/models/emploment_info/Employee.java
@@ -0,0 +1,47 @@
+package com.example.hrms.models.emploment_info;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@Table(name = "employee")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long employeeId;
+
+    @Column(unique = true)
+    private Long code;
+
+    @Column(unique = true)
+    private Long panNumber;
+
+    private Long oldEmployeeCode;
+
+    private Date dateOfAppointment;
+
+    private String bioMetricId;
+
+    private Salutation salutation;
+    public enum Salutation {
+        MR, MRS, MISS, SHRIMAN, SHRIMATI, DR
+    }
+
+//    @ManyToOne
+//    @JoinColumn(referencedColumnName = "departmentId", name = "departmentId")
+//    private Department department;
+
+    private String firstName;
+
+    private String middleName;
+
+    private String lastName;
+
+    private String unit;
+
+
+}
