09cd1b257d0487c5fc848092e0975ebc7fb4da99
rushabhbhatiya21 <rushabhbhatiya1997@gmail.com>
2024-02-27 11:45:55 +0530

    1144

--- src/main/java/com/example/hrms/models/emploment_info/Photograph.java
+++ src/main/java/com/example/hrms/models/emploment_info/Photograph.java
@@ -7,9 +7,7 @@ import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.Base64;
-import java.util.Objects;
 
 @Entity
 @Getter
